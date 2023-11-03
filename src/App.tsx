cd3daae6bd5e5ee84645ee8505dc06756ffadd42
Mateus Santos <a.mateussm1.5@gmail.com>
2023-11-03 20:26:32 -0300

    error TS6133: 'React' is declared but its value is never read.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent } from 'react';
 
 function App(): JSX.Element {
   const [file, setFile] = useState<File | null>(null);
